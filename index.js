d518d736d25e8d30377d63952196f04a9579c0b1
Micaela Frank <micaelalfrank@gmail.com>
2022-04-25 15:52:02 -0400

    commit tree 9413baad75f11e1bb8b74ea5037f9fc307c4846e

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = "Micaela Frank";
+const firstInitial = currentUser[0];
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${firstInitial}!`;
